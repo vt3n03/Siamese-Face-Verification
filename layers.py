7088e9398d14a1244069df40269d584cc1e49cf0
Victor <v.tan.victor@gmail.com>
2025-12-09 22:38:04 -0500

    clean up

--- layers.py
+++ layers.py
@@ -8,5 +8,6 @@ class L1Dist(Layer):
     def __init__(self, **kwargs):
         super().__init__()
        
+
     def call(self, input_embedding, validation_embedding):
         return tf.math.abs(input_embedding - validation_embedding)
